5b694f6287af87d49b9df05f56f0fdf5a9e45b1d
bilal-younus <bilalyounus33@gmail.com>
2023-02-08 12:59:18 +0500

    all the components and file structure complete

---
+++ src/component/MovieListing/MovieListing.js
@@ -0,0 +1,11 @@
+import React from 'react' ;
+
+export const MovieListing = () => {
+    return (
+        <>
+            <div>
+                MovieListing
+            </div>
+        </>
+    )
+}
